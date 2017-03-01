a20fccbe18e7a7afd267e4d5c2474ff9946bc229
zeneli <tud27246@temple.edu>
2017-03-01 15:20:26 -0500

    Add `userIsInTheMiddleOfTyping` check to remove prepended 0.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,10 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        let textCurrentlyInDisplay = display!.text!
-        display!.text = textCurrentlyInDisplay + digit
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
 
 }
